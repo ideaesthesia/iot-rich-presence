c9903be467433d0a6301a1352d62522dfc5422b3
ideaesthesia <79393986+ideaesthesia@users.noreply.github.com>
2021-02-21 16:01:42 +0700

    Add files via upload

--- README.md
+++ README.md
@@ -1,14 +1,10 @@
 ## Chung
 
-<img src="https://cdn.discordapp.com/attachments/785144177457102881/812927114063642685/unknown.png"/>
+<img src="https://cdn.discordapp.com/attachments/785144177457102881/812927114063642685/unknown.png" width="20%" />
 iot-rich-presence thêm tính năng hiển thị bạn đang chơi IOT. Phần mở rộng Chrome là cần thiết để đưa dữ liệu URL về app chính
 
 ## Cài đặt
-Tải về file *.exe và *.zip từ mục Releases. Để cài phần mở rộng Chrome, giải nén file *.zip (dùng lựa chọn Extract to extensions/) sau đó
-* Vào chrome ://extensions
-* Bật chế độ dành cho nhà phát triển
-* Bấm nút Tải tiện ích đã giải nén
-* Chọn thư mục "extensions" vừa giải nén
+Tải về file *.crx và *.exe từ trang [Releases](), sau đó mở chrome://extensions, bật chế độ dành cho nhà phát triển và kéo file *.crx vừa tải về vào cửa sổ Chrome
 
 ## Cách dùng
 Mở file IOTrpc.exe vừa tải về. Phần mở rộng phải được cài từ trước và Discord phải được mở từ trước.
--- app.js
+++ app.js
@@ -1,8 +1,69 @@
 const express = require('express');
 const RPC = require("discord-rpc");
+const { app, BrowserWindow, Tray, Menu } = require('electron')
+const path = require('path');
+const url = require('url');
 
 const client = new RPC.Client({ transport: 'ipc' });
 client.login({ clientId: '811855112929673246' });
+const iconPath = path.join(__dirname, 'IOTLogo.ico')
+
+var isQuitting
+
+function createWindow () {
+  const win = new BrowserWindow({
+    width: 800,
+    height: 600,
+    webPreferences: {
+      nodeIntegration: true
+    },
+	icon: iconPath
+  })
+
+  win.loadFile('index.html')
+  win.on('minimize', (event) => {
+    event.preventDefault();
+    win.hide();
+  });
+
+  win.on('close', (event) => {
+    if(!isQuitting){
+      event.preventDefault();
+      win.hide();
+    }
+    return false;
+  });
+  let tray = null
+  tray = new Tray(iconPath)
+  const contextMenu = Menu.buildFromTemplate([
+	{ label: 'Show App', click:  function(){
+        win.show();
+		} 
+	},
+    { label: 'Quit', click:  function(){
+        isQuitting = true;
+        app.quit();
+		}
+	}
+  ])
+  tray.setToolTip('Rich Presence for IOT')
+  tray.setContextMenu(contextMenu)
+}
+
+app.whenReady().then(createWindow)
+
+app.on('window-all-closed', () => {
+  if (process.platform !== 'darwin') {
+    app.quit()
+  }
+})
+
+app.on('activate', () => {
+  if (BrowserWindow.getAllWindows().length === 0) {
+    createWindow()
+  }
+})
+
 
 client.once('ready', ()=>{
 	const app = express();
